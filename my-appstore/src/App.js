a338a9d5904bac9bbc75a7f213b61d1fadc79144
Marcos Bautista P <macondo777@hotmail.com>
2022-10-20 09:15:46 -0500

    configuración inicial del proeyecto. version 1.0

---
+++ my-appstore/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hola mundo desde React!!!.</h1>
+        <h3>Dummies web. misión tic 2022.</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
